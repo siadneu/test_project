5da4f4eefaf925affa9cff85a185ec1554136930
Igor Sednev <siadnyov@gmail.com>
2021-11-25 20:09:06 +0300

    remove credentials

--- backend/app/model/data_model/aws/s3.py
+++ backend/app/model/data_model/aws/s3.py
@@ -4,13 +4,9 @@ import os
 
 class S3:
     def __init__(self, aws_access_key_id=None, aws_secret_access_key=None, region_name=None):
-        self.aws_access_key_id = aws_access_key_id or os.getenv("AWS_ACCESS_KEY_ID")
-        self.aws_secret_access_key = aws_secret_access_key or os.getenv("AWS_SECRET_ACCESS_KEY")
         self.region_name = region_name or os.getenv("AWS_REGION_NAME")
         self.client = boto3.client(
             's3',
-            aws_access_key_id=self.aws_access_key_id,
-            aws_secret_access_key=self.aws_secret_access_key,
             region_name=self.region_name
         )
 
